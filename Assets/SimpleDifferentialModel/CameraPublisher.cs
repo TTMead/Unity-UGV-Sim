85626de57a45fe62ef0a6e1664907c2fb6596af2
TTMead <timothy.mead20@gmail.com>
2023-09-10 12:18:17 +1000

    Fixes and refactoring to image streaming

--- Assets/SimpleDifferentialModel/CameraPublisher.cs
+++ Assets/SimpleDifferentialModel/CameraPublisher.cs
@@ -11,95 +11,61 @@ public class CameraPublisher : MonoBehaviour
 {
     public float fps_target = 10f;
 
-    private RenderTexture renderTexture;
-
-    private const int isBigEndian = 0;
-    private const int step = 4;
-
+    private RenderTexture beboopPovRenderTexture;
     private float last_send_time;
-
     private Camera cameraComp;
 
     void Start()
     {
-        cameraComp = GetComponent<Camera>();
-
-        // Render texture 
-        renderTexture = cameraComp.targetTexture;
-        // renderTexture = new RenderTexture(cameraComp.pixelWidth, cameraComp.pixelHeight, 24, UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_SRGB);
-        // renderTexture.Create();
+        // Extract the render texture for this camera
+        cameraComp = GetComponent<Camera>();   
+        beboopPovRenderTexture = cameraComp.targetTexture;
         
+        // Initialise image publisher
         ROSConnection.GetOrCreateInstance().RegisterPublisher<RosMessageTypes.Sensor.ImageMsg>("/front_camera");
 
         last_send_time = 0f;
-
     }
 
     void Update()
     {
+        // Queue screenshots to be published at a desired fps target rate
         if (Time.time - last_send_time > (1/fps_target)) {
-            SendImage();
+            StartCoroutine(QueueScreenshot());
             last_send_time = Time.time;
         }
     }
 
-
-    private void SendImage()
-    {
-        // cameraComp.targetTexture = renderTexture;
-        RenderTexture currentRT = RenderTexture.active;
-        RenderTexture.active = renderTexture;
-        cameraComp.Render();
-
-        Texture2D mainCameraTexture = new Texture2D(renderTexture.width, renderTexture.height);
-        mainCameraTexture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
-        mainCameraTexture.Apply();
-        RenderTexture.active = currentRT;
-
-        // Convert screenshot to image message
-        RosMessageTypes.Std.HeaderMsg header = new RosMessageTypes.Std.HeaderMsg();
-        header.stamp.sec = (int)Time.time;
-        header.stamp.nanosec = (uint)(Time.time*1e9);
-        header.frame_id = "camera_link";
-        ImageMsg imageMsg = mainCameraTexture.ToImageMsg(header);
-
-        // publish
-        ROSConnection.GetOrCreateInstance().Publish("/front_camera", imageMsg);
-    }
-
-    
-    private void PublishImage(byte[] imageData)
-    {
-        uint imageHeight = (uint)renderTexture.height;
-        uint imageWidth = (uint)renderTexture.width;
-
-        RosMessageTypes.Std.HeaderMsg header = new RosMessageTypes.Std.HeaderMsg();
-        header.stamp.sec = (int)Time.time;
-        header.stamp.nanosec = (uint)(Time.time*1e9);
-        header.frame_id = "camera_link";
-
-        RosMessageTypes.Sensor.ImageMsg rosImage = new RosMessageTypes.Sensor.ImageMsg(header, imageHeight, imageWidth, "rgba8", isBigEndian, step, imageData);
-        ROSConnection.GetOrCreateInstance().Publish("/front_camera", rosImage);
-    }
-
     /// <summary>
-    ///     Capture the main camera's render texture and convert to bytes.
+    /// Waits for the end of a frame before taking a screenshot and publishing the image over ROS
     /// </summary>
-    /// <returns>imageBytes</returns>
-    private byte[] CaptureScreenshot()
+    private IEnumerator QueueScreenshot()
     {
-        Camera.main.targetTexture = renderTexture;
-        RenderTexture currentRT = RenderTexture.active;
-        RenderTexture.active = renderTexture;
-        Camera.main.Render();
-        Texture2D mainCameraTexture = new Texture2D(renderTexture.width, renderTexture.height);
-        mainCameraTexture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
-        mainCameraTexture.Apply();
-        RenderTexture.active = currentRT;
-        // Get the raw byte info from the screenshot
-        byte[] imageBytes = mainCameraTexture.GetRawTextureData();
-        Camera.main.targetTexture = null;
-        return imageBytes;
+        yield return new WaitForEndOfFrame();
+
+        // Set the current render texture to our Beboop-pov camera
+        RenderTexture.active = beboopPovRenderTexture;
+
+        // Create Texture2D
+        Texture2D screenshotTexture = new Texture2D(beboopPovRenderTexture.width, beboopPovRenderTexture.height);
+
+        // Copy the pixels of the current renderer onto the texture 
+        screenshotTexture.ReadPixels(new Rect(0, 0, beboopPovRenderTexture.width, beboopPovRenderTexture.height), 0, 0);
+        screenshotTexture.Apply();
+
+        // Release the current render texture
+        RenderTexture.active = null;
+
+        // Create ROS header
+        RosMessageTypes.Std.HeaderMsg header = new RosMessageTypes.Std.HeaderMsg();
+        header.stamp.sec = (int)Time.time;
+        header.stamp.nanosec = (uint)(Time.time*1e9);
+        header.frame_id = "camera_link";
+
+        // Convert screenshot to image message with header
+        ImageMsg imageMsg = screenshotTexture.ToImageMsg(header);
+
+        ROSConnection.GetOrCreateInstance().Publish("/front_camera", imageMsg);
     }
 
 }
